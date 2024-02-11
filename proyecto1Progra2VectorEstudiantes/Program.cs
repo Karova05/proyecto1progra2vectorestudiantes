840f5ef2c76fcc6b8afd38ab895662c84b8f6478
karla <karova05@yahoo.com>
2024-02-10 19:23:30 -0600

    proyecto progra

---
+++ proyecto1Progra2VectorEstudiantes/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int opcion = 0;
+
+do
+{
+
+    Console.Clear();
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+
+    Console.WriteLine("************ UNIVERSIDAD HISPANOAMERICANA **********************");
+
+    Console.WriteLine("************ DEPARTAMENTO DE ADMISION **********************");
+
+    Console.WriteLine("1- inicializar vectores");
+
+    Console.WriteLine("2- incluir estudiantes");
+
+    Console.WriteLine("3- consultar estudiantes");
+
+    Console.WriteLine("4- modificar estudiantes");
+
+    Console.WriteLine("5- eliminar estudiantes");
+
+    Console.WriteLine("6- submenu reportes");
+
+    Console.WriteLine("7- salir");
+
+
+    Console.ForegroundColor = ConsoleColor.Blue;
+
+    Console.WriteLine("que desea hacer");
+
+    opcion = int.Parse(Console.ReadLine());
+
+    if (opcion==1)
+    {
+        Console.WriteLine("inicializar vectores");
+    }
+
+    else if (opcion == 2)
+
+    {
+        Console.WriteLine("incluir estudiantes");  
+    }
+
+    else if (opcion == 3)
+
+    {
+        Console.WriteLine("consultar estudiantes");  
+
+    }
+    else if (opcion == 4)
+
+    {
+        Console.WriteLine("modificar estudiantess");  
+    }
+
+    else if (opcion == 5)
+
+    {
+        Console.WriteLine("eliminar estudiantes");  
+
+    }
+
+    else if (opcion == 6)
+
+    {
+        Console.WriteLine("submenu reportes");  
+
+    }
+
+    else if (opcion == 7)
+
+    {
+        Console.WriteLine("salir");  
+
+    }
+
+} while (opcion < 1 || opcion > 7);
+
+Console.ForegroundColor = ConsoleColor.White;
